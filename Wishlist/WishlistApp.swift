8c648dcea9918587db9708c31eb5c478b8aaf0b4
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-20 17:46:13 -0600

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Isaac Urbina on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
