d9d668d99988af6cc02277407957ce26264530ee
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-24 16:00:36 -0600

    42. 6. How to delete data with SwiftData

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -23,6 +23,11 @@ struct ContentView: View {
 					Text(wish.title)
 						.font(.title.weight(.light))
 						.padding(.vertical, 2)
+						.swipeActions {
+							Button("Delete", role: .destructive) {
+								modelContext.delete(wish)
+							}
+						}
 				}
 			}
 			.navigationTitle("Wishlist")
@@ -33,6 +38,12 @@ struct ContentView: View {
 						isAlertShowing.toggle()
 					} label: {
 						Image(systemName: "plus").imageScale(.large)
+					}
+				}
+				
+				if !wishes.isEmpty {
+					ToolbarItem(placement: .bottomBar) {
+						Text("\(wishes.count) wish\(wishes.count > 1 ? "es" : "")")
 					}
 				}
 			}
